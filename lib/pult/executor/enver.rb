092f79d9bdd13faac9624541d1e6ea6f0c5ac236
dmitryck <dmitryck@gmail.com>
2019-09-04 17:57:59 +0300

    enver (ref, add): PULT_ENV_BASE and PULT_ENV_DEFAULT env pattern, with_env meth RESOLVER;

--- lib/pult/executor/enver.rb
+++ lib/pult/executor/enver.rb
@@ -1,31 +1,62 @@
 module Pult::Executor::Enver
 
-  # TODO
-  DEFENV = ENV.to_h
+  CURRENT = ENV.to_h
 
-  def with_env env=ENV.to_h, env_base=DEFENV
-    env_delete_diff! env, env_base
+  ENVS = ENV['PULT_ENV_DEFAULT'] || ENV['PULT_ENV_BASE']
+
+  ENV_BASE = !!ENV['PULT_ENV_BASE']
+
+  DEFAULT_VARS = ENVS&.split(/=.+?"?\n/)
+
+  DEFAULT = DEFAULT_VARS ?
+    CURRENT.select{|k, v| DEFAULT_VARS.include?(k) } : CURRENT
+
+  RESOLVER = {
+    default: DEFAULT,
+    current: CURRENT
+  }
+
+  def with_env resolver_or_env
+    env = RESOLVER[resolver_or_env] || resolver_or_env
     
-    yield
-    
-    env_set_diff! env, env_base
-  end
-
-  def env_delete_diff! env, env_base
-    env_diff(env, env_base).each do |var|
-      ENV.delete(var)
-    end
-  end
-
-  def env_set_diff! env, env_base
-    env_diff(env, env_base).each do |var|
-      ENV[var] = env[var]
+    if env.is_a?(Hash)
+      ENV_BASE ? env_set!(env) : env_set(env)
+      
+      yield
+      
+      env_set!(CURRENT)
+    else
+      raise ArgumentError, 'Env not resolved'
     end
   end
 
   private
 
-  def env_diff env, env_base
-    env_base.keys - env.keys
+  def env_set env
+    ENV.keys.each do |var|
+      ENV[var] = env[var] if env[var]
+    end
+  end
+
+  def env_set! env
+    ENV.keys.each do |var|
+      env[var] ? ENV[var] = env[var] : ENV.delete(var)
+    end
+  end
+
+  def env_delete_diff! env1, env2
+    diff(env1, env2).each do |var|
+      ENV.delete(var)
+    end
+  end
+
+  def env_set_diff! env1, env2
+    diff(env1, env2).each do |var|
+      ENV[var] = env[var]
+    end
+  end
+
+  def diff env1, env2
+    env2.keys - env1.keys
   end
 end
