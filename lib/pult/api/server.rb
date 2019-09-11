09013dca89251e70d0477dc2a49e7c6262d19f46
dmitryck <dmitryck@gmail.com>
2019-09-11 20:02:30 +0300

    api, req-lib (temp fix): curl -X POST (and no -d "");

--- lib/pult/api/server.rb
+++ lib/pult/api/server.rb
@@ -2,6 +2,10 @@ module Pult::Api::Server
 
   PORT = ENV['PULT_API_PORT']&.to_i || 9292
 
+  # temp fix curl -d ''
+  METHS = WEBrick::HTTPRequest::BODY_CONTAINABLE_METHODS
+  WEBrick::HTTPRequest::BODY_CONTAINABLE_METHODS = METHS - ['POST']
+
   def self.run! api:, port: PORT
     Rack::Handler::WEBrick.run api, Port: port
   end
